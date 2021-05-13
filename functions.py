20a624282c9436fdc1d71401685cab6a54a0b5bd
BreathingCat <bosquearenas@hotmail.com>
2021-05-13 13:06:09 +0200

    Added tests

---
+++ functions.py
@@ -0,0 +1,9 @@
+# File containing simple dummy functions to run testing on
+
+def sum_test(a, b):
+    return a + b
+
+
+def sub_test(a, b):
+    return a - b
+
---
+++ tests/tests.py
@@ -0,0 +1,12 @@
+import unittest
+import functions
+
+
+class TestBatter(unittest.TestCase):
+
+    def test_sum(self):
+        self.assertEqual(3, functions.sum_test(1, 2))
+
+
+if __name__ == '__main__':
+    unittest.main()
