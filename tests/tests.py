d67cd7ce9724732282beac581f4ec081616047f2
BreathingCat <bosquearenas@hotmail.com>
2021-05-13 13:50:12 +0200

    Added nosetests

--- tests/tests.py
+++ tests/tests.py
@@ -7,6 +7,9 @@ class TestBatter(unittest.TestCase):
     def test_sum(self):
         self.assertEqual(3, functions.sum_test(1, 2))
 
+    def test_sum2(self):
+        self.assertEqual(3, functions.sum_test(1, 2))
+
 
 if __name__ == '__main__':
     unittest.main()
